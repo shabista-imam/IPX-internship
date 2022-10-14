44be7aa946a554720f5c5bf36abc3c102e31b45d
shabista-imam <64741363+shabista-imam@users.noreply.github.com>
2022-10-14 15:36:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,210 @@
+const data = [
+    {
+      "_id": "e214e17b-2f75-41a9-be54-6e4474c9c3c7",
+      "nftCategory": "0",
+      "coverName": "Glitch",
+      "coverFileUrl": "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80 ",
+      "shortDescription": "A video-game developer (Lucas Neff) falls for his lawyer's (Levi Fiehler) girlfriend (Maiara Walsh), jeopardizing his lawsuit, career and relationship with his best friend (Blake Silver)",
+      "participants": {
+        "director": "Prasanth Varma",
+        "cast": "Navdeep A",
+        "music": "Senthil Sri Prasad"
+      },
+      "gallery": [
+        {
+          "fileName": "file_description_img",
+          "fileUrl": "https://wallpaperaccess.com/full/3910748.jpg"
+        },
+        {
+          "fileName": "file_description_img",
+          "fileUrl": "https://png.pngtree.com/thumb_back/fw800/background/20200425/pngtree-glitch-style-abstract-black-background-image_334609.jpg"
+        }
+      ],
+      "ownerName": "IPX",
+      "owner": "0xfE40F6eaeCB6900B0a871356F31976fDd1eFaD68",
+      "projectIndividualNfts": [
+        {
+          "purchaseList": [
+            {
+              "payoutHistories": [],
+              "ipxNftPurchaseRefUuid": "c3d42bec-a318-45b5-98f8-8c1aee7aa065",
+              "owner": "0xc00fcd3eb9fe7479bf44eab6d92dd29f1aa86c2a",
+              "purchasedOn": "August 18th 2022, 4:24:51 pm",
+              "purchasedPrice": 150,
+              "equityPercentage": 10,
+              "transactionRefId": "866204b7-66dc-48c6-ab8e-6bc47500189f",
+              "blockChainDetails": {
+                "ipfsFileUrl": "https://gateway.pinata.cloud/ipfs/QmZZ9hJcPcWB8mtvYW8vRgnBP23nFFjzYo4VK1qnVSFKaU",
+                "ipfsMetaDataJson": "https://gateway.pinata.cloud/ipfs/QmehDk82x2PaTN7V77zPtwHWHMcith8dPSSthYKhUtpikG",
+                "mintedNftTransactionHash": "0x76cc3d6be77cbe9a552a1e8883988619310ce88367fc1ab5e169a90bab2e50d4",
+                "nftTokenId": 16,
+                "nftOwnerTransferTransactionHash": "0x67f6cc3505aac5acccf6c0821f027442888817e41007a5abaa88fa90482ab454"
+              }
+            },
+            {
+              "payoutHistories": [],
+              "ipxNftPurchaseRefUuid": "6e4b53fa-303f-408a-abf1-0190bc9e5c6c",
+              "owner": "0xc00fcd3eb9fe7479bf44eab6d92dd29f1aa86c2a",
+              "purchasedOn": "August 22nd 2022, 6:39:46 am",
+              "purchasedPrice": 150,
+              "equityPercentage": 10,
+              "transactionRefId": "7a38eec0-abe2-4b99-9808-7ce3143a7c1a",
+              "blockChainDetails": {
+                "ipfsFileUrl": "https://gateway.pinata.cloud/ipfs/QmZZ9hJcPcWB8mtvYW8vRgnBP23nFFjzYo4VK1qnVSFKaU",
+                "ipfsMetaDataJson": "https://gateway.pinata.cloud/ipfs/QmRfYs1aaUKPxyR7YLXJWuNccNqNjUhL19rkyeGxHCFYM5",
+                "mintedNftTransactionHash": "0xb3c9c1e1e755fec3aaf102ff4d9906ec39511787aa071469a9fe7129f3f6a64b",
+                "nftTokenId": 17,
+                "nftOwnerTransferTransactionHash": "0x38ce8417d09411aee53951b15d32fa2632ee1edd64b20ac2756d0c762861ebf5"
+              }
+            }
+          ],
+          "gallery": [
+            {
+              "fileName": "file_description_img",
+              "fileUrl": "https://assets.entrepreneur.com/content/3x2/2000/20191009140007-GettyImages-1053962188.jpeg"
+            },
+            {
+              "fileName": "file_description_img",
+              "fileUrl": "https://assets.entrepreneur.com/content/3x2/2000/20191009140007-GettyImages-1053962188.jpeg"
+            }
+          ],
+          "_id": "8a657a98-e2d5-47a7-a34b-4d398232b57d",
+          "createdAt": "2022-07-29T07:17:53.721Z",
+          "deleted": false,
+          "projectId": "e214e17b-2f75-41a9-be54-6e4474c9c3c7",
+          "projectIndividualNftId": "1405da21-fe8b-4c6b-b501-984a26f4560d",
+          "coverName": "Camera Man NFT",
+          "coverFileName": "vintage-cinema-concept-vector",
+          "coverFileUrl": "https://149695847.v2.pressablecdn.com/wp-content/uploads/2019/07/investor-banner-01.jpg",
+          "owner": "0xc00fcd3eb9fe7479bf44eab6d92dd29f1aa86c2a",
+          "ownerName": "Vinay S",
+          "price": 150,
+          "qty": 9,
+          "equityPercentage": 10,
+          "isAuction": false
+        },
+        {
+          "purchaseList": [
+            {
+              "payoutHistories": [],
+              "ipxNftPurchaseRefUuid": "2a08ae7f-2817-4437-8843-565f2e79fd98",
+              "owner": "0xc00fcd3eb9fe7479bf44eab6d92dd29f1aa86c2a",
+              "purchasedOn": "July 29th 2022, 10:42:57 am",
+              "purchasedPrice": null,
+              "equityPercentage": 15,
+              "transactionRefId": "bd8d3434-601c-4f32-94dc-f7ac3cf283f7",
+              "blockChainDetails": {
+                "ipfsFileUrl": "https://gateway.pinata.cloud/ipfs/QmWdRheQQ22yf7qEizDDxLjgbP4PviUiGG8a9sxpPJrD5C",
+                "ipfsMetaDataJson": "https://gateway.pinata.cloud/ipfs/QmXtkrZEe1VDckz2ZSYxkAi8GhV4ZwAXQYo8Mqsp7wVeKE",
+                "mintedNftTransactionHash": "0x5a5774a318296cc542b66500b208a581c29fdfbb78b0d82e56d445ce189bd1ad",
+                "nftTokenId": 11,
+                "nftOwnerTransferTransactionHash": "0x8d3ccbb720409d26c606ba318473f6d5b664ecd6faf16f15ace885102a3afe92"
+              }
+            }
+          ],
+          "gallery": [
+            {
+              "fileName": "file_description_img",
+              "fileUrl": "https://assets.entrepreneur.com/content/3x2/2000/20191009140007-GettyImages-1053962188.jpeg"
+            },
+            {
+              "fileName": "file_description_img",
+              "fileUrl": "https://assets.entrepreneur.com/content/3x2/2000/20191009140007-GettyImages-1053962188.jpeg"
+            }
+          ],
+          "_id": "8c5011b5-9376-4b38-b8da-23e431e3b6fa",
+          "createdAt": "2022-07-29T07:18:17.942Z",
+          "deleted": false,
+          "projectId": "e214e17b-2f75-41a9-be54-6e4474c9c3c7",
+          "projectIndividualNftId": "32a2373f-a7ef-470f-967d-62e674a1850b",
+          "coverName": "Co Producer NFT",
+          "coverFileName": "vintage-cinema-producer-vector",
+          "coverFileUrl": "https://media.istockphoto.com/vectors/film-shoot-vector-id481807477?k=20&m=481807477&s=612x612&w=0&h=7C-vl_gpEzSesCKyNUmANkg9SKgPjPmEX0gaFBBbQ8Y=",
+          "owner": "0xc00fcd3eb9fe7479bf44eab6d92dd29f1aa86c2a",
+          "ownerName": "Vinay S",
+          "price": 0,
+          "qty": 10,
+          "equityPercentage": 15,
+          "isAuction": true,
+          "nftAuction": {
+            "expiry": "2022-07-30T18:55:00+00:00",
+            "startingBidPrice": 100,
+            "lastBidPrice": 210,
+            "recentBidder": "0xc00fcd3eb9fe7479bf44eab6d92dd29f1aa86c2a",
+            "recentBidDate": "July 29th 2022, 7:58:39 am",
+            "nftAuctionHistories": [
+              {
+                "bidPaymentsTransactionSummary": [
+                  {
+                    "_id": "80c507d4-10ce-4d60-be3d-f96d15848758",
+                    "createdAt": "2022-07-29T07:57:58.532Z",
+                    "deleted": false,
+                    "userId": "662c80e9-03dc-4a39-b6aa-7992f849c91c",
+                    "email": "vinay@ipx.club",
+                    "walletAddress": "0xc00fcd3eb9fe7479bf44eab6d92dd29f1aa86c2a",
+                    "projectId": "e214e17b-2f75-41a9-be54-6e4474c9c3c7",
+                    "projectIndividualNftId": "32a2373f-a7ef-470f-967d-62e674a1850b",
+                    "nftName": "Co Producer NFT",
+                    "image": "https://media.istockphoto.com/vectors/film-shoot-vector-id481807477?k=20&m=481807477&s=612x612&w=0&h=7C-vl_gpEzSesCKyNUmANkg9SKgPjPmEX0gaFBBbQ8Y=",
+                    "status": "PENDING",
+                    "usdc": {
+                      "amountInUsdc": "0.1250",
+                      "usdcInrConversionRate": 0.0125,
+                      "gasPrice": 99000000000,
+                      "metamaskPostResponse": {
+                        "usdcContractAddress": "0xe11A86849d99F524cAC3E7A0Ec1241828e332C62",
+                        "fromUserAddress": "0xc00fcd3eb9fe7479bf44eab6d92dd29f1aa86c2a",
+                        "toIpxAddress": "0xfE40F6eaeCB6900B0a871356F31976fDd1eFaD68",
+                        "amountInUsdcCoinNotion": "0.1250",
+                        "amountToSend": "1bc16d674ec8000",
+                        "gasPrice": 99000000000
+                      }
+                    },
+                    "paymentMode": "USDC",
+                    "basePrice": 100,
+                    "bidPayableAmount": 10,
+                    "pendingPayableAmount": 200,
+                    "isAuction": true,
+                    "bidPrice": 210
+                  }
+                ],
+                "owner": "0xc00fcd3eb9fe7479bf44eab6d92dd29f1aa86c2a",
+                "bidPrice": 210,
+                "name": "Vinay S",
+                "email": "vinay@ipx.club",
+                "phone": "+919566996990",
+                "userId": "662c80e9-03dc-4a39-b6aa-7992f849c91c",
+                "bidDate": "July 29th 2022, 7:58:39 am"
+              }
+            ]
+          },
+          "expired": true
+        }
+      ],
+      "qty": 0
+    }
+  ]
+
+    const elementColl = document?.getElementsByClassName("card-image")
+    console.log(elementColl)
+    function setImagetoAllCards(){
+        console.log("working")
+        console.log(elementColl)
+       let arr = Array.prototype.slice.call(elementColl)
+        console.log(arr)
+        for (let i = 0; i < arr.length; i++) {
+            const card = arr[i];
+            console.log(card)
+            card.src = data[0].coverFileUrl
+        }
+        console.log(arr[0])
+        const nameArrcoll = document.getElementsByClassName("card-name")
+        let nameArr = [...nameArrcoll]
+        console.log(nameArr)
+        nameArr.forEach((name) => {
+            name.innerHTML = data[0].ownerName
+        })
+    }
+    
+setImagetoAllCards()
+    // arrofelement?.forEach((card) => card.src = data[
